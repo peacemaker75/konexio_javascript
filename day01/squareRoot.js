093f295ba761793583788040de7570e317905e1e
bilel ayachi <daskik75@gmail.com>
2020-06-11 18:45:14 +0200

    update exercice2

---
+++ day01/squareRoot.js
@@ -0,0 +1,11 @@
+function squareRoot(num){
+    var num;
+    for (var i = num; i > 1; i--) {
+        if (i * i === num) {
+            return i;
+        }
+    }
+    return 0;
+  }
+  console.log(squareRoot(13));
+  console.log(squareRoot(81));
