6e45ab3e3ce90cdf5e07760249b1efade622f657
bilel ayachi <daskik75@gmail.com>
2020-06-22 18:02:20 +0200

    update monday

--- day01/passwordGen.js
+++ day01/passwordGen.js
@@ -1,3 +1,16 @@
+var alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var result = '';
 function passwordGen(num) {
+    if (num < 6 || num > 15){
+        console.log('erreur')
+    }else {
+        for(var i = 0; i < num; i++) {
+        var random = Math.floor(Math.random()*alphabet.length);
+        var letter = alphabet[random];
+        result = result + letter;
+        }return result;
+    }
     
 }
+
+console.log(passwordGen(11));
---
+++ day01/wordCount.js
@@ -0,0 +1,5 @@
+function wordCount(str) {
+    result = str.split(' ');
+    return result.length;
+}
+console.log(wordCount('IL Y A BEAUCOUP DE CAUSES POUR LESQUELLES JE SUIS PRÊT À MOURIR, MAIS AUCUNE POUR LAQUELLE JE SUIS PRÊT À TUER'));
