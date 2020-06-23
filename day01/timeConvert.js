3f267787f74b1fad50146b79fde445c4d769a503
bilel ayachi <daskik75@gmail.com>
2020-06-23 19:48:13 +0200

    update tuesday3

--- day01/timeConvert.js
+++ day01/timeConvert.js
@@ -1,23 +1,10 @@
 function timeConvert(num) {
-    var rest = num;
-    var result = '';
-    var nbJour = Math.floor(rest/(3600*24));
-    rest -= nbJour*24*3600;
-    var nbHour = Math.floor(rest/3600);
-    rest -= nbHour*60;
+    var nbHour = Math.floor(num/3600);
+    var rest = num - (nbHour*3600);
     var nbMinute = Math.floor(rest/60);
-    rest -= nbMinute*60;
-    var nbSecond = rest;
-    if (nbJour >0)
-        result = result+nbJour+':';
-    if (nbHour >0)
-        result = result+nbHour+':';
-    if (nbMinute >0)
-        result = result+nbMinute+':';
-    if (nbSecond >0)
-        result = result+nbSecond+'';
+    rest = rest- (nbMinute*60);
     
-    return result;
+    return nbHour + ':' + nbMinute + ':' + rest;
 
 }
-console.log(timeConvert(3700));
+console.log(timeConvert(11003));
